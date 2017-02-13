a5189c1707be15f26c715b1ba8a1e4cad4b5674e
Wilson Guimarães <wegneto@gmail.com>
2017-02-13 17:22:29 +0000

    Aula 55: Iniciando o Atendimento ao Cliente

--- JEE/curso-jee7/curso-jee7-war/src/br/com/devmedia/cursojee/control/OrcamentoController.java
+++ JEE/curso-jee7/curso-jee7-war/src/br/com/devmedia/cursojee/control/OrcamentoController.java
@@ -1,6 +1,8 @@
 package br.com.devmedia.cursojee.control;
 
+import br.com.devmedia.cursojee.entities.Cliente;
 import br.com.devmedia.cursojee.entities.Orcamento;
+import br.com.devmedia.cursojee.entities.OrcamentoServico;
 import br.com.devmedia.cursojee.service.OrcamentoService;
 import java.util.List;
 import javax.ejb.EJB;
@@ -18,6 +20,64 @@ public class OrcamentoController extends BasicControl {
     
     private List<Orcamento> orcamentos;
     
-    //teste
+    private Cliente cliente;
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public OrcamentoService getService() {
+        return service;
+    }
+
+    public void setService(OrcamentoService service) {
+        this.service = service;
+    }
+
+    public Orcamento getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Orcamento selected) {
+        this.selected = selected;
+    }
+
+    public List<Orcamento> getOrcamentos() {
+        return orcamentos;
+    }
+
+    public void setOrcamentos(List<Orcamento> orcamentos) {
+        this.orcamentos = orcamentos;
+    }
+    
+    public String atenderCliente(Cliente cliente) {
+       setCliente(cliente);
+       cleanCache();
+       
+       return "/restrito/orcamento.faces";
+    }
+   
+    public String getOrcamentoItens(Orcamento orcamento) {
+        StringBuilder sb = new StringBuilder();
+        for (OrcamentoServico item : orcamento.getOrcamentoServicoList()) {
+            if (sb.length() > 0) {
+                sb.append(", ");
+            }
+            sb.append(item.getServico().getNome());
+        }
+        
+        return sb.toString();
+    }
+    
+
+    private void cleanCache() {
+        setSelected(new Orcamento());
+        getSelected().setCliente(getCliente());
+        setOrcamentos(service.getOrcamentos(getCliente().getId()));
+    }
     
 }
